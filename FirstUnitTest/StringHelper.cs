ca44867b68181dfbd44b1466056254f1ff6061a2
alibozlak <ali-bozlak@outlook.com>
2024-01-07 00:23:50 +0300

    Add project files.

---
+++ FirstUnitTest/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace Helpers
+{
+    public class StringHelper
+    {
+        //Gereksinimler : 
+        //1. Girilen ifadenin başındaki ve sonundaki fazla boşluklar silinmelidir.
+        //2. Girilen ifadenin içindeki fazla boşluklar silinmelidir.
+        public static string FazlaBosluklariSil(string text)
+        {
+            text = text.Trim();
+            string newText = string.Empty;
+            for (int i = 0; i < text.Length; i++)
+            {
+                if (text[i] == ' ' && text[i+1]== ' ')
+                {
+                    continue;
+                }
+                newText += text[i];
+            }
+            return newText;
+        }
+    }
+}
---
+++ HelpersTests/StringHelperTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Helpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helpers.Tests
+{
+    [TestClass()]
+    public class StringHelperTests
+    {
+        [TestMethod()]
+        public void BasindakiVeSonundakiBosluklariSilTest()
+        {
+            //Arrange
+            string text = "     Engin Demiroğ                  ";
+            string expected = "Engin Demiroğ";
+
+            //Actual
+            string actualized = StringHelper.FazlaBosluklariSil(text);
+
+            //Assert
+            Assert.AreEqual(expected, actualized);
+        }
+
+        [TestMethod()]
+        public void AradakiBosluklariSil()
+        {
+            string text = "Bu   metnin    arasındaki       fazla   boşluklar       silinmeli";
+            string expected = "Bu metnin arasındaki fazla boşluklar silinmeli";
+
+            string actualized = StringHelper.FazlaBosluklariSil(text);
+
+            Assert.AreEqual(expected, actualized);
+        }
+    }
+}
